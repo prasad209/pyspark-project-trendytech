b9f7a0f664044d8725457a424ae70551400099c4
Prasad <79194792+prasad209@users.noreply.github.com>
2024-08-06 11:48:17 +0000

    teardown included in testing

--- conftest.py
+++ conftest.py
@@ -3,6 +3,15 @@
 import pytest
 from lib.Utils import get_spark_session
 
+#the code till yield(inclusive) will run till test cases and after that 
+#the part below yield i.e stop session here will work 
+
+#setup code is == > code before yield 
+#teardown == > code after yield == > releasing the resources
+
+
 @pytest.fixture
 def spark():
-    return get_spark_session("LOCAL")
+    spark_session= get_spark_session("LOCAL")
+    yield spark_session
+    spark_session.stop()
